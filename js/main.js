1618091a9c593c985386d08576308309e8e0b3cf
Timdavidcole <timdavidcole@gmail.com>
2020-03-13 21:28:35 +0000

    Added basic table generation

--- js/main.js
+++ js/main.js
@@ -10,5 +10,39 @@ function genTableHead(table, tableHeads) {
   });
 }
 
+function genTableBody(table, tableBody) {
+  tableBody.forEach(member => {
+    const row = table.insertRow();
+    Object.keys(member).forEach(key => {
+      let cell, text;
+      switch (key) {
+        case "lastName":
+          cell = row.insertCell();
+          text = document.createTextNode(isAcceptedName(member));
+          cell.appendChild(text);
+          break;
+        case "email":
+          cell = row.insertCell();
+          text = document.createTextNode(member[key]);
+          cell.appendChild(text);
+          break;
+        case "perms":
+          cell = row.insertCell();
+          text = document.createTextNode(member[key]);
+          cell.appendChild(text);
+        default:
+          break;
+      }
+    });
+  });
+}
+
+function isAcceptedName(member) {
+  return member.accepted
+    ? `${member["firstName"]} ${member["lastName"]}`
+    : "Pending acceptance";
+}
+
 let membersTable = document.getElementById("members-table-main");
 genTableHead(membersTable, tableHeaders);
+genTableBody(membersTable, exampleTeamMembers);
