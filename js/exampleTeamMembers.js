198c8d47f8c466baf2bc785dae8a6c7f97607de4
Timdavidcole <timdavidcole@gmail.com>
2020-03-13 20:12:11 +0000

    added example team members array

---
+++ js/exampleTeamMembers.js
@@ -0,0 +1,50 @@
+const exampleTeamMembers = [
+  {
+    firstName: "Erica",
+    lastName: "Badu",
+    email: "e.badu@example.com",
+    perms: "owner",
+    profilePicUrl: "erica-profile-icon.png",
+    accepted: true
+  },
+  {
+    firstName: "Pat",
+    lastName: "Nelsson",
+    email: "p.nelsson@example.com",
+    perms: "admin",
+    profilePicUrl: "p-profile-icon.png",
+    accepted: true
+  },
+  {
+    firstName: "",
+    lastName: "",
+    email: "j.dog@example.com",
+    perms: "standard",
+    profilePicUrl: "",
+    accepted: false
+  },
+  {
+    firstName: "Amy",
+    lastName: "Namy",
+    email: "a.namy@example.com",
+    perms: "standard",
+    profilePicUrl: "amy-profile-icon.png",
+    accepted: true
+  },
+  {
+    firstName: "Victor",
+    lastName: "D.",
+    email: "v.d@example.com",
+    perms: "standard",
+    profilePicUrl: "v-profile-icon.png",
+    accepted: true
+  },
+  {
+    firstName: "Olly",
+    lastName: "",
+    email: "o.hunter@example.com",
+    perms: "standard",
+    profilePicUrl: "olly-profile-icon.png",
+    accepted: true
+  }
+];
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+const exampleTeamMembers = [
+  {
+    firstName: "Erica",
+    lastName: "Badu",
+    email: "e.badu@example.com",
+    perms: "owner",
+    profilePicUrl: "erica-badu-profile-icon.png"
+  }
+];
