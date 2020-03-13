ce010641857a789dd92427b0888dbf4fb3d04355
Timdavidcole <timdavidcole@gmail.com>
2020-03-13 23:36:26 +0000

    added useless delete button

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ function genTableBody(table, tableBody) {
           text = document.createTextNode(member[key]);
           cell.appendChild(text);
           cell.setAttribute("id", `memberPerms${index}`);
+          addDeleteButton(cell)
         default:
           break;
       }
@@ -46,14 +47,23 @@ function addProfilePic(member, cell) {
   const profilePic = document.createElement("i");
   profilePic.setAttribute(
     "style",
-    `background-image: url('./img/${isAcceptedProfilePic(
-      member
-    )}')`
+    `background-image: url('./img/${isAcceptedProfilePic(member)}')`
   );
-  profilePic.setAttribute(
-    "class",'profile-pic'
-  );
+  profilePic.setAttribute("class", "profile-pic");
   cell.insertBefore(profilePic, cell.childNodes[0]);
+}
+
+function addDeleteButton(cell) {
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("class", "delete-button");
+  const deleteButtonIcon = document.createElement("i");
+  deleteButtonIcon.setAttribute(
+    "style",
+    `background-image: url('./img/bin.png')`
+  );
+  deleteButtonIcon.setAttribute("class", "delete-button-icon");
+  deleteButton.appendChild(deleteButtonIcon)
+  cell.appendChild(deleteButton);
 }
 
 let membersTable = document.getElementById("members-table-main");
