d6cb6725f3d1265ff20f34a0ed27020719081864
Timdavidcole <timdavidcole@gmail.com>
2020-03-14 00:19:17 +0000

    Delete button now fully working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let teamMembers = [...exampleTeamMembers];
+
 function genTableHead(table, tableHeads) {
   const thead = table.createTHead();
   const row = thead.insertRow();
@@ -12,34 +14,42 @@ function genTableHead(table, tableHeads) {
 
 function genTableBody(table, tableBody) {
   const tBody = table.createTBody();
+  tBody.setAttribute("id", `membersTable`);
+
   tableBody.forEach((member, index) => {
     const row = tBody.insertRow();
-    Object.keys(member).forEach(key => {
-      let cell, text;
-      switch (key) {
-        case "lastName":
-          cell = row.insertCell();
-          text = document.createTextNode(isAcceptedName(member));
-          cell.appendChild(text);
-          cell.setAttribute("id", `memberName${index}`);
-          addProfilePic(member, cell);
-          break;
-        case "email":
-          cell = row.insertCell();
-          text = document.createTextNode(member[key]);
-          cell.appendChild(text);
-          cell.setAttribute("id", `memberEmail${index}`);
-          break;
-        case "perms":
-          cell = row.insertCell();
-          text = document.createTextNode(member[key]);
-          cell.appendChild(text);
-          cell.setAttribute("id", `memberPerms${index}`);
-          addDeleteButton(cell)
-        default:
-          break;
-      }
-    });
+    genTableRow(row, member, index);
+  });
+}
+
+function genTableRow(row, member, index) {
+  row.setAttribute("class", `member-row`);
+  row.setAttribute("id", `memberRow${index}`);
+  Object.keys(member).forEach(key => {
+    let cell, text;
+    switch (key) {
+      case "lastName":
+        cell = row.insertCell();
+        text = document.createTextNode(isAcceptedName(member));
+        cell.appendChild(text);
+        cell.setAttribute("id", `memberName${index}`);
+        addProfilePic(member, cell);
+        break;
+      case "email":
+        cell = row.insertCell();
+        text = document.createTextNode(member[key]);
+        cell.appendChild(text);
+        cell.setAttribute("id", `memberEmail${index}`);
+        break;
+      case "perms":
+        cell = row.insertCell();
+        text = document.createTextNode(member[key]);
+        cell.appendChild(text);
+        cell.setAttribute("id", `memberPerms${index}`);
+        addDeleteButton(cell, row, index);
+      default:
+        break;
+    }
   });
 }
 
@@ -53,19 +63,26 @@ function addProfilePic(member, cell) {
   cell.insertBefore(profilePic, cell.childNodes[0]);
 }
 
-function addDeleteButton(cell) {
+function addDeleteButton(cell, row, index) {
   const deleteButton = document.createElement("button");
   deleteButton.setAttribute("class", "delete-button");
+  deleteButton.setAttribute("onclick", `deleteMember(${index})`);
   const deleteButtonIcon = document.createElement("i");
   deleteButtonIcon.setAttribute(
     "style",
     `background-image: url('./img/bin.png')`
   );
   deleteButtonIcon.setAttribute("class", "delete-button-icon");
-  deleteButton.appendChild(deleteButtonIcon)
+  deleteButton.appendChild(deleteButtonIcon);
   cell.appendChild(deleteButton);
+}
+
+function deleteMember(index) {
+  teamMembers.splice(index, 1);
+  const row = document.getElementById(`memberRow${index}`)
+  row.parentNode.removeChild(row);
 }
 
 let membersTable = document.getElementById("members-table-main");
 genTableHead(membersTable, tableHeaders);
-genTableBody(membersTable, exampleTeamMembers);
+genTableBody(membersTable, teamMembers);
