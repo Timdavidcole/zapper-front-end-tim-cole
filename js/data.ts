c89fd03610670bc3e2607d197af95ae97a6f9b2d
Timdavidcole <timdavidcole@gmail.com>
2020-03-14 15:32:13 +0000

    Fixed many typescript errors and tweaked name cell padding

---
+++ js/data.ts
@@ -0,0 +1,52 @@
+const tableHeaders: Array<string> = ["Team member", "Email", "Permission level"];
+
+const exampleTeamMembers: Array<Member> = [
+  {
+    firstName: "Erica",
+    lastName: "Badu",
+    email: "e.badu@example.com",
+    perms: "Owner",
+    profilePicUrl: "erica-profile-icon.png",
+    accepted: true
+  },
+  {
+    firstName: "Pat",
+    lastName: "Nelsson",
+    email: "p.nelsson@example.com",
+    perms: "Admin",
+    profilePicUrl: "p-profile-icon.png",
+    accepted: true
+  },
+  {
+    firstName: "",
+    lastName: "",
+    email: "j.dog@example.com",
+    perms: "Standard",
+    profilePicUrl: "",
+    accepted: false
+  },
+  {
+    firstName: "Amy",
+    lastName: "Namy",
+    email: "a.namy@example.com",
+    perms: "Standard",
+    profilePicUrl: "amy-profile-icon.png",
+    accepted: true
+  },
+  {
+    firstName: "Victor",
+    lastName: "D.",
+    email: "v.d@example.com",
+    perms: "Standard",
+    profilePicUrl: "v-profile-icon.png",
+    accepted: true
+  },
+  {
+    firstName: "Olly",
+    lastName: "",
+    email: "o.hunter@example.com",
+    perms: "Standard",
+    profilePicUrl: "olly-profile-icon.png",
+    accepted: true
+  }
+];
